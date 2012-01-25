05a2bb7f43baf704f738fd17f5996fa48cfcce1b
jocelynmeilleur <jocelynm_ap203@hotmail.com>
2012-01-25 00:57:20 -0500

    Mon premier commit Git

---
+++ Test1/src/test1/Test1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author admin
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!"); 
+    }
+}
